ecb0d3772222f1bfb58bf4e2585f9f0400e93d55
LDC7 <lihwar1@gmail.com>
2017-05-31 17:52:48 +0400

    M->W

---
+++ HWT_01/Task01/Program.cs
@@ -0,0 +1,136 @@
+﻿namespace Task01
+{
+    using System;
+
+    /*
+    Написать консольное приложение, которое проверяет
+    принадлежность точки заштрихованной области.
+    Пользователь вводит координаты точки (x; y) и выбирает
+    букву графика(a-к). В консоли должно высветиться сообщение:
+    «Точка[(x; y)] принадлежит фигуре[г]».
+    */
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите координату x: ");
+            double x;
+            while (!double.TryParse(Console.ReadLine(), out x))
+            {
+                Console.Write("Ошибка!\nВведите координату x: ");
+            }
+
+            Console.Write("Введите координату y: ");
+            double y;
+            while (!double.TryParse(Console.ReadLine(), out y))
+            {
+                Console.Write("Ошибка!\nВведите координату y: ");
+            }
+
+            Console.Write("Введите букву графика(а-к): ");
+            int graphic = Console.Read();
+            
+            if (CheckPointEntrance(x, y, (char)graphic))
+            {
+                Console.WriteLine("Точка ({0}; {1}) принадлежит фигуре {2}", x, y, (char)graphic);
+            }
+            else
+            {
+                Console.WriteLine("Точка ({0}; {1}) не принадлежит фигуре {2}", x, y, (char)graphic);
+            }
+
+            Console.ReadKey();
+        }
+
+        private static bool CheckPointEntrance(double x, double y, char graphic)
+        {
+            switch (graphic)
+            {
+                case 'а':
+                    // круг
+                    if (1 >= x * x + y * y)
+                    {
+                        return true;
+                    }
+
+                    break;
+                case 'б':
+                    // окружность
+                    if ((1 >= x * x + y * y) && (0.25 <= x * x + y * y))
+                    {
+                        return true;
+                    }
+
+                    break;
+                case 'в':
+                    // квадрат
+                    if ((1 >= x) && (-1 <= x) && (1 >= y) && (-1 <= y))
+                    {
+                        return true;
+                    }
+
+                    break;
+                case 'г':
+                    // ромб
+                    if ((y <= x + 1) && (y <= -1 * x + 1) && (y >= x - 1) && (y >= -1 * x - 1))
+                    {
+                        return true;
+                    }
+
+                    break;
+                case 'д':
+                    // ромб
+                    if ((y / 2 <= x + 0.5) && (y / 2 <= -1 * x + 0.5) && (y / 2 >= x - 0.5) && (y / 2 >= -1 * x - 0.5))
+                    {
+                        return true;
+                    }
+
+                    break;
+                case 'е':
+                    // полукруг и треугольник
+                    if ((y * 2 <= x + 2) && (y * 2 >= -1 * x - 2) && ((x <= 0) || (1 >= x * x + y * y)))
+                    {
+                        return true;
+                    }
+
+                    break;
+                case 'ж':
+                    // треугольник
+                    if ((y >= -1) && (y / 2 <= x + 1) && (y / 2 <= -1 * x + 1))
+                    {
+                        return true;
+                    }
+
+                    break;
+                case 'з':
+                    if (((y <= x) || ((x < 0) && (y <= -1 * x))) && (y >= -2) && (x >= -1) && (x <= 1))
+                    {
+                        return true;
+                    }
+
+                    break;
+                case 'и':
+                    if ((y * 3 - 1 <= x + 2) && (((y <= 0) && (x >= 0)) || ((y / 2 <= x + 1.5) && (y <= -1 * x))))
+                    {
+                        return true;
+                    }
+
+                    break;
+                case 'к':
+                    if ((y >= 1) || ((y >= x) && (y - 1 >= -1 * x - 1)))
+                    {
+                        return true;
+                    }
+
+                    break;
+
+                default:
+                    Console.WriteLine("Ошибка. График не найден");
+                    break;
+            }
+
+            return false;
+        }
+    }
+}
---
+++ HWT_02/Task03/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace Task03
+{
+    using System;
+
+    /*
+    Написать программу, которая запрашивает с клавиатуры число N и выводит на экран следующее «изображение», состоящее из N строк.
+    */
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+
+            Console.Write("Введите N: ");
+            while (!int.TryParse(Console.ReadLine(), out n))
+            {
+                Console.Write("Некорректное значение!\nВведите N: ");
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n - i - 1; j++)
+                {
+                    Console.Write(' ');
+                }
+
+                for (int j = 0; j < ((2 * i) + 1); j++)
+                {
+                    Console.Write('*');
+                }
+
+                Console.Write('\n');
+            }
+
+            Console.ReadKey();
+        }
+    }
+}

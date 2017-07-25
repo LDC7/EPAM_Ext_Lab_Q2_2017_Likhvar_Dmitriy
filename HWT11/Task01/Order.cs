c2f2bff0e61e9e4688400fc8b4873a71360ecc40
LDC7 <lihwar1@gmail.com>
2017-07-25 20:15:09 +0400

    Создан проект

---
+++ HWT11/Task01/Order.cs
@@ -0,0 +1,22 @@
+﻿namespace Task01
+{
+    using System;
+
+    class Order
+    {
+        public int? OrderID;
+        public int? CustomerID;
+        public int? EmployeeID;
+        public DateTime OrderDate;
+        public DateTime RequiredDate;
+        public DateTime ShippedDate;
+        public int? ShipVia;
+        public decimal? Freight;
+        public string ShipName;
+        public string ShipAddress;
+        public string ShipCity;
+        public string ShipRegion;
+        public string ShipPostalCode;
+        public string ShipCountry;
+    }
+}
